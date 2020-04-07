7fad8413f4c3a777a718fbf5d637725091e710fa
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-04-07 09:53:52 -0300

    adding a prints to configure mysql and phpmyadmin

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -6,5 +6,15 @@ cd ~
 sudo apt-get install -y mysql-server
 sudo mysql_secure_installation
 
+echo -e "\n ############################# \n"
+echo -e "RUN THE FOLLOWING COMMANDS TO \n CONFIGURE MYSQL FOR PHPMYADMIN \n"
+echo -e "sudo mysql -u root -p"
+echo -e "use mysql;"
+echo -e "update user set authentication_string=PASSWORD('') where User='root';"
+echo -e "update user set plugin='mysql_native_password' where User='root';"
+echo -e "flush privileges;"
+echo -e "quit;"
+echo -e "\n ############################# \n"
+
 cd -
 echo -e "\n ### END SCRIPT MYSQL ### \n"
